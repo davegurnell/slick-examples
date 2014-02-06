d19684367f88b03a4fae9c8bd0eb1753e1be6980
Dave Gurnell <dave@untyped.com>
2014-02-06 18:25:57 +0000

    Added `TableAndMappedProjection` and comments

--- src/main/scala/com/typesafe/slick/examples/lifted/FoxChickenGrainExample.scala
+++ src/main/scala/com/typesafe/slick/examples/lifted/FoxChickenGrainExample.scala
@@ -1,17 +1,30 @@
 package com.typesafe.slick.examples.lifted
 
-// Use H2Driver to connect to an H2 database
 import scala.slick.driver.H2Driver.simple._
-import scala.slick.lifted.MappedProjection
+import scala.slick.ast.{ Node, Symbol }
+import scala.slick.lifted.{ AbstractTable, ColumnBase, MappedProjection }
 
-/**
- * A simple example that uses statically typed queries against an in-memory
- * H2 database. The example data comes from Oracle's JDBC tutorial at
- * http://download.oracle.com/javase/tutorial/jdbc/basics/tables.html.
- */
 object FoxChickenGrainExample extends App {
 
   // Case classes -------------------------------
+
+  // This example maps three database tables, `Fox`, `Chicken`, and `Grain`,
+  // and models two left-join queries on the tables:
+  //
+  //   - `ChickenGrainQuery` is equivalent to `Chicken leftJoin Grain on ...`
+  //   - `FoxChickenGrainQuery` is equivalent to `Fox leftJoin (Chicken  leftJoin Grain on ...) on ...`
+  //
+  // The aim is to allow the queries to be easily composed while also making
+  // each query individually useful to the programmer:
+  //
+  //   `ChickenGrainQuery where (_.chicken.name === "Chicken Little") foreach { (record: ChickenGrain) => ... }`
+  //
+  // We're attempting to do this using a class called `TableAndMappedProjection` to implement a `?`
+  // method on each table that allows us to select an option of it from the database while
+  // still allowing things like `_.table.field` in join conditions.
+  //
+  // The solution we have works on plain queries, but throws a runtime exception
+  // when using `_.table.field`. See the last lines of the file for the failing case.
 
   case class Fox(id: Int, name: String, chickenId: Option[Int])
   case class Chicken(id: Int, name: String, grainId: Option[Int])
@@ -20,19 +33,45 @@ object FoxChickenGrainExample extends App {
   case class ChickenGrain(chicken: Chicken, grain: Option[Grain])
   case class FoxChickenGrain(fox: Fox, chicken: Option[Chicken], grain: Option[Grain])
 
+  // OptionTable --------------------------------
+
+  // This is an attempt to create a data structure that behaves like a MappedProjection but
+  // keeps a referenced to the original table to allow the developer to refer to the columns
+  // involved by name.
+  //
+  // The problem with this approach appears to be that the columns in the table aren't
+  // necessarily the same ones in the mapped projection. See the definition of `?` in any of
+  // the tables below for an example.
+
+  case class TableAndMappedProjection[Table <: AbstractTable[_], Unpacked, Packed](
+    table: Table,
+    projection: MappedProjection[Unpacked, Packed]) extends ColumnBase[Unpacked] {
+
+    type Projection = MappedProjection[Unpacked, Packed]
+
+    type Self = TableAndMappedProjection[_, _, _]
+    override def toString = s"TableAndMappedProjection($table, $projection)"
+    override def toNode: Node = projection.toNode
+    def encodeRef(path: List[Symbol]) = TableAndMappedProjection(table.encodeRef(path), projection.encodeRef(path))
+  }
+
   // Tables -------------------------------------
+
+  // We define the tables using `TableAndMappedProjection` to provide a `?` method:
 
   class FoxTable(tag: Tag) extends Table[Fox](tag, "FOX") {
     def id = column[Int]("ID")
     def name = column[String]("NAME")
     def chickenId = column[Option[Int]]("CHICKEN_ID")
     def * = (id, name, chickenId) <> (Fox.tupled, Fox.unapply)
-    def ? = (id.?, name.?, chickenId).shaped <> (
-      { row => row._1.map(_ => Fox(row._1.get, row._2.get, row._3)) },
-      { (_: Any) => sys.error("Can't insert into optional projection")})
+    def ? = TableAndMappedProjection(
+      this,
+      (id.?, name.?, chickenId).shaped <> (
+        { row => row._1.map(_ => Fox(row._1.get, row._2.get, row._3)) },
+        { (_: Any) => sys.error("Can't insert into optional projection")}))
   }
 
-  type OptionalFox = MappedProjection[Option[Fox],(Option[Int], Option[String], Option[Int])]
+  type OptionalFox = TableAndMappedProjection[FoxTable, Option[Fox], (Option[Int], Option[String], Option[Int])]
 
   val FoxQuery = TableQuery[FoxTable]
 
@@ -41,12 +80,12 @@ object FoxChickenGrainExample extends App {
     def name = column[String]("NAME")
     def grainId = column[Option[Int]]("GRAIN_ID")
     def * = (id, name, grainId) <> (Chicken.tupled, Chicken.unapply)
-    def ? = (id.?, name.?, grainId).shaped <> (
+    def ? = TableAndMappedProjection(this, (id.?, name.?, grainId).shaped <> (
       { row => row._1.map(_ => Chicken(row._1.get, row._2.get, row._3)) },
-      { (_: Any) => sys.error("Can't insert into optional projection")})
+      { (_: Any) => sys.error("Can't insert into optional projection")}))
   }
 
-  type OptionalChicken = MappedProjection[Option[Chicken],(Option[Int], Option[String], Option[Int])]
+  type OptionalChicken = TableAndMappedProjection[ChickenTable, Option[Chicken], (Option[Int], Option[String], Option[Int])]
 
   val ChickenQuery = TableQuery[ChickenTable]
 
@@ -54,16 +93,37 @@ object FoxChickenGrainExample extends App {
     def id = column[Int]("ID")
     def name = column[String]("NAME")
     def * = (id, name) <> (Grain.tupled, Grain.unapply)
-    def ? = (id.?, name.?).shaped <> (
-      { row => row._1.map(_ => Grain(row._1.get, row._2.get)) },
-      { (_: Any) => sys.error("Can't insert into optional projection")})
+    def ? = TableAndMappedProjection(
+      this,
+      (id.?, name.?).shaped <> (
+        { row => row._1.map(_ => Grain(row._1.get, row._2.get)) },
+        { (_: Any) => sys.error("Can't insert into optional projection")}))
   }
 
-  type OptionalGrain = MappedProjection[Option[Grain],(Option[Int], Option[String])]
+  type OptionalGrain = TableAndMappedProjection[GrainTable, Option[Grain], (Option[Int], Option[String])]
 
   val GrainQuery = TableQuery[GrainTable]
 
+  // Aggregates ---------------------------------
+
+  // We define aggregate queries involving the tables:
+
+  lazy val ChickenGrainQuery =
+    ChickenQuery
+      .leftJoin(GrainQuery)
+      .on((chickenRow, grainRow) => chickenRow.grainId === grainRow.id)
+      .map { case (chicken, grain) => (chicken, grain.?) }(chickenGrainShape)
+
+  lazy val FoxChickenGrainQuery =
+    FoxQuery
+      .leftJoin(ChickenGrainQuery)
+      .on((foxRow, chickenGrainRow) => foxRow.chickenId === chickenGrainRow._1.id)
+      .map { case (fox, (chicken, optGrain)) => (fox, chicken.?, optGrain) }(foxChickenGrainShape)
+
   // Shapes -------------------------------------
+
+  // Finally, we define shapes that allow us to retrieve the `FoxChickenGrain`
+  // and `ChickenGrain` case classes directly from the results of each query:
 
   final class FoxChickenGrainShape[Level <: ShapeLevel](val shapes: Seq[Shape[_, _, _, _]])
     extends MappedScalaProductShape[Level, Product, (FoxTable, OptionalChicken, OptionalGrain), FoxChickenGrain, (FoxTable, OptionalChicken, OptionalGrain)] {
@@ -85,12 +145,13 @@ object FoxChickenGrainExample extends App {
   final class ChickenGrainShape[Level <: ShapeLevel](val shapes: Seq[Shape[_, _, _, _]])
     extends MappedScalaProductShape[Level, Product, (ChickenTable, OptionalGrain), ChickenGrain, (ChickenTable, OptionalGrain)] {
 
-    def buildValue(elems: IndexedSeq[Any]) =
+    def buildValue(elems: IndexedSeq[Any]) = {
       if(elems(0).isInstanceOf[ChickenTable]) {
         (elems(0).asInstanceOf[ChickenTable], elems(1).asInstanceOf[OptionalGrain])
       } else {
         ChickenGrain(elems(0).asInstanceOf[Chicken], elems(1).asInstanceOf[Option[Grain]])
       }
+    }
     def copy(shapes: Seq[Shape[_, _, _, _]]) = new ChickenGrainShape(shapes)
   }
 
@@ -98,23 +159,11 @@ object FoxChickenGrainExample extends App {
     ChickenQuery.unpackable.shape,
     GrainQuery.unpackable.shape))
 
-  // Aggregates ---------------------------------
-
-  val ChickenGrainQuery =
-    ChickenQuery
-      .leftJoin(GrainQuery)
-      .on((chickenRow, grainRow) => chickenRow.grainId === grainRow.id)
-      .map { case (chicken, grain) => (chicken, grain.?) }(chickenGrainShape)
-
-  val FoxChickenGrainQuery =
-    FoxQuery
-      .leftJoin(ChickenGrainQuery)
-      .on((foxRow, chickenGrainRow) => foxRow.chickenId === chickenGrainRow._1.id)
-      .map { case (fox, (chicken, optGrain)) => (fox, chicken.?, optGrain) }(foxChickenGrainShape)
-
   // Application main ---------------------------
 
   Database.forURL("jdbc:h2:mem:test2", driver = "org.h2.Driver") withSession { implicit session =>
+
+    // Create test data -------------------------
 
     (FoxQuery.ddl ++ ChickenQuery.ddl ++ GrainQuery.ddl).create
 
@@ -133,20 +182,27 @@ object FoxChickenGrainExample extends App {
       Grain(1, "Grain 1")
     )
 
+    // Actual test code -------------------------
+
+    // This works fine, showing that the queries produce results of the correct type:
+
     println()
     println("ChickenGrainQuery:")
-    for(chickenGrain <- ChickenGrainQuery) {
-      println(chickenGrain)
-    }
+    ChickenGrainQuery foreach (println)
 
-    // println()
-    // println("Filtered ChickenGrainQuery:")
-    // ChickenGrainQuery where { case (c, g) => g.name === "Grain 1" } foreach { println }
+    // This works fine, showing that the queries can be nested:
 
     println()
     println("FoxChickenGrainQuery:")
-    for(foxChickenGrain <- FoxChickenGrainQuery) {
-      println(foxChickenGrain)
-    }
+    FoxChickenGrainQuery foreach (println)
+
+    // This fails because `grain.table.name` isn't actually selected as part of the query
+    // (the optional column in `grain.projection` is being selected instead):
+
+    println()
+    println("Filtered ChickenGrainQuery:")
+    ChickenGrainQuery where { case (chicken: ChickenTable, grain: OptionalGrain) =>
+      grain.table.name === "Grain 1"
+    } foreach (println)
   }
 }
